5c91ecf337a9d2cb22260a0d561c201053115a29
Thiago von Sydow <thiagovs05@gmail.com>
2012-08-18 14:51:17 -0300

    Reconhecimento facial funcionando

---
+++ obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,140 @@
+/home/thiago/opt/workspaceAndroid/AndroidRobotCamera/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ /home/thiago/opt/workspaceAndroid/AndroidRobotCamera/jni/DetectionBasedTracker_jni.cpp \
+ /home/thiago/opt/workspaceAndroid/AndroidRobotCamera/jni/DetectionBasedTracker_jni.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/jni.h \
+ /home/thiago/opt/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ /home/thiago/opt/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/assert.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdlib.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/string.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/malloc.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/alloca.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/strings.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/types.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdint.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/types.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/types.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/memory.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/math.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/limits.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/limits.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/limits.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/internal_types.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/limits.h \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/syslimits.h \
+ /home/thiago/opt/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /home/thiago/opt/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ /home/thiago/opt/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ /home/thiago/opt/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ /home/thiago/opt/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ /home/thiago/opt/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ /home/thiago/opt/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ /home/thiago/opt/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ /home/thiago/opt/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ /home/thiago/opt/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /home/thiago/opt/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp \
+ /home/thiago/opt/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ /home/thiago/opt/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/deque \
+ /home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/android/log.h
+
+/home/thiago/opt/workspaceAndroid/AndroidRobotCamera/jni/DetectionBasedTracker_jni.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/jni.h:
+
+/home/thiago/opt/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+/home/thiago/opt/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/assert.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdlib.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/string.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/alloca.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/strings.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/types.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdint.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/types.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/types.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/memory.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/math.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/limits.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/limits.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/limits.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/internal_types.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/limits.h:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/syslimits.h:
+
+/home/thiago/opt/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/home/thiago/opt/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+/home/thiago/opt/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+/home/thiago/opt/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+/home/thiago/opt/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+/home/thiago/opt/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+/home/thiago/opt/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+/home/thiago/opt/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/home/thiago/opt/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+/home/thiago/opt/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/home/thiago/opt/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp:
+
+/home/thiago/opt/OpenCV-2.4.2-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+/home/thiago/opt/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/deque:
+
+/home/thiago/opt/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/android/log.h:
